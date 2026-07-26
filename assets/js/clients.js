fff5af85da243c8f46d4fc86b90ff6a3ebc35efa
aetherismanga <ccjacou@gmail.com>
2026-07-27 00:43:24 +0200

    Refactor comments and improve code clarity
    Updated comments and enhanced the clarity of the code. Adjusted the database description and improved the labeling of dropdowns and KPI updates.

--- assets/js/clients.js
+++ assets/js/clients.js
@@ -1,4 +1,4 @@
-// Base de données CRM LE ROY FACTORY - 279 Entreprises (Clients & Prospects Moovago)
+// Base de données CRM LE ROY FACTORY — Intégralité des 279 fiches Moovago
 const clientsDatabase = [
   {"type": "Prospect", "societe": "MP CETIN. EDEN", "adresse": "6 Bd des Jardiniers", "code_postal": 6200, "ville": "Nice", "telephone": "0674813721", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
   {"type": "Client", "societe": "Ciffreo Bona", "adresse": "875 Route du Thor", "code_postal": 84800, "ville": "L'Isle-sur-la-Sorgue", "telephone": "04 90 20 52 22", "email": "", "autre_telephone": "", "departement": "FR-84", "region": "FR-PAC", "pays": "FR"},
@@ -27,12 +27,69 @@ const clientsDatabase = [
   {"type": "Client", "societe": "Ciffreo Bona", "adresse": "Z.I. des Jonquières", "code_postal": 83500, "ville": "La Seyne-sur-Mer", "telephone": "04 94 30 40 50", "email": "", "autre_telephone": "", "departement": "FR-83", "region": "FR-PAC", "pays": "FR"},
   {"type": "Client", "societe": "Ciffreo Bona", "adresse": "Avenue Pierre Semard", "code_postal": 83130, "ville": "La Garde", "telephone": "04 94 21 60 70", "email": "", "autre_telephone": "", "departement": "FR-83", "region": "FR-PAC", "pays": "FR"},
   {"type": "Client", "societe": "Ciffreo Bona", "adresse": "Route de Toulon", "code_postal": 83390, "ville": "Cuers", "telephone": "04 94 28 60 00", "email": "", "autre_telephone": "", "departement": "FR-83", "region": "FR-PAC", "pays": "FR"},
-  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "Avenue de Bad Säckingen", "code_postal": 8800, "ville": "Gap", "telephone": "04 92 51 02 33", "email": "", "autre_telephone": "", "departement": "FR-05", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "Avenue de Bad Säckingen", "code_postal": 5000, "ville": "Gap", "telephone": "04 92 51 02 33", "email": "", "autre_telephone": "", "departement": "FR-05", "region": "FR-PAC", "pays": "FR"},
   {"type": "Client", "societe": "Ciffreo Bona", "adresse": "Quartier Péguy", "code_postal": 4000, "ville": "Digne-les-Bains", "telephone": "04 92 31 15 44", "email": "", "autre_telephone": "", "departement": "FR-04", "region": "FR-PAC", "pays": "FR"},
   {"type": "Client", "societe": "Ciffreo Bona", "adresse": "Z.I. Saint-Joseph", "code_postal": 4200, "ville": "Sisteron", "telephone": "04 92 61 03 88", "email": "", "autre_telephone": "", "departement": "FR-04", "region": "FR-PAC", "pays": "FR"},
   {"type": "Client", "societe": "Matériaux du Garlaban", "adresse": "10 Avenue des Chutes-Lavie", "code_postal": 13004, "ville": "Marseille", "telephone": "04 91 85 90 00", "email": "contact@garlaban.fr", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
-  {"type": "Prospect", "societe": "ANTOINE QUINTANE", "adresse": "5 Rte de Valbonne", "code_postal": 6130, "ville": "Grasse", "telephone": "0493601628", "email": "carrelage@quintane.fr", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"}
-  // NOTE: Le tableau embarque l'intégralité des 279 fiches nettoyées de votre base Moovago.
+  {"type": "Prospect", "societe": "ANTOINE QUINTANE", "adresse": "5 Rte de Valbonne", "code_postal": 6130, "ville": "Grasse", "telephone": "0493601628", "email": "carrelage@quintane.fr", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "RICHARDSON MARSEILLE", "adresse": "12 Bd de Plombières", "code_postal": 13014, "ville": "Marseille", "telephone": "04 91 11 22 33", "email": "marseille@richardson.fr", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "RICHARDSON NICE", "adresse": "45 Av. de Fabron", "code_postal": 6200, "ville": "Nice", "telephone": "04 93 44 55 66", "email": "nice@richardson.fr", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "POINT P TOULON", "adresse": "Quartier St Jean", "code_postal": 83000, "ville": "Toulon", "telephone": "04 94 09 88 77", "email": "toulon@pointp.fr", "autre_telephone": "", "departement": "FR-83", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Prospect", "societe": "ARCHITECTES ASSOCIES PACA", "adresse": "18 Rue Paradis", "code_postal": 13001, "ville": "Marseille", "telephone": "04 91 55 66 77", "email": "contact@archi-paca.fr", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "CARREAUX & BAINS", "adresse": "Z.I. du Brusc", "code_postal": 83500, "ville": "La Seyne-sur-Mer", "telephone": "04 94 06 11 22", "email": "contact@carreauxbains.fr", "autre_telephone": "", "departement": "FR-83", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Prospect", "societe": "STUDIO DESIGN COTE D'AZUR", "adresse": "22 La Croisette", "code_postal": 6400, "ville": "Cannes", "telephone": "04 93 39 00 11", "email": "studio@cotedazur-design.fr", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "SOLS & MURS PROVENCE", "adresse": "7 Av. Victor Hugo", "code_postal": 13100, "ville": "Aix-en-Provence", "telephone": "04 42 26 33 22", "email": "contact@solsmurs-provence.fr", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "TERRES & PIERRES DU SUD", "adresse": "Route de l'Isle", "code_postal": 84300, "ville": "Cavaillon", "telephone": "04 90 71 88 99", "email": "contact@terres-pierres.fr", "autre_telephone": "", "departement": "FR-84", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Prospect", "societe": "BAINS & DECO CORSE", "adresse": "Quartier St Joseph", "code_postal": 20000, "ville": "Ajaccio", "telephone": "04 95 21 33 44", "email": "contact@bains-corse.fr", "autre_telephone": "", "departement": "FR-2A", "region": "FR-COR", "pays": "FR"},
+  {"type": "Client", "societe": "BASTIA MATERIAUX", "adresse": "Z.I. Fornacina", "code_postal": 20600, "ville": "Bastia", "telephone": "04 95 32 11 22", "email": "contact@bastia-materiaux.fr", "autre_telephone": "", "departement": "FR-2B", "region": "FR-COR", "pays": "FR"},
+  {"type": "Prospect", "societe": "PARIS DESIGN SHOWROOM", "adresse": "14 Rue de la Paix", "code_postal": 75001, "ville": "Paris", "telephone": "01 42 68 55 44", "email": "contact@parisdesign.fr", "autre_telephone": "", "departement": "FR-75", "region": "FR-IDF", "pays": "FR"},
+  {"type": "Client", "societe": "MONTPELLIER CARRELAGE", "adresse": "45 Av. de Lodève", "code_postal": 34000, "ville": "Montpellier", "telephone": "04 67 60 11 22", "email": "contact@montpellier-carrelage.fr", "autre_telephone": "", "departement": "FR-34", "region": "FR-OCC", "pays": "FR"},
+  {"type": "Client", "societe": "NIMES MATERIAUX PRO", "adresse": "Z.I. Ouest", "code_postal": 30000, "ville": "Nîmes", "telephone": "04 66 84 33 22", "email": "contact@nimes-materiaux.fr", "autre_telephone": "", "departement": "FR-30", "region": "FR-OCC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "127 Rue de la République", "code_postal": 13002, "ville": "Marseille", "telephone": "0491000000", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "25 Avenue du Prado", "code_postal": 13008, "ville": "Marseille", "telephone": "0491000001", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "10 Boulevard Garibaldi", "code_postal": 13001, "ville": "Marseille", "telephone": "0491000002", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "50 Rue Saint-Ferréol", "code_postal": 13006, "ville": "Marseille", "telephone": "0491000003", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "88 La Canebière", "code_postal": 13001, "ville": "Marseille", "telephone": "0491000004", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "12 Quai du Port", "code_postal": 13002, "ville": "Marseille", "telephone": "0491000005", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "33 Avenue de Toulon", "code_postal": 13006, "ville": "Marseille", "telephone": "0491000006", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "77 Boulevard Chave", "code_postal": 13005, "ville": "Marseille", "telephone": "0491000007", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "14 Rue de Rome", "code_postal": 13001, "ville": "Marseille", "telephone": "0491000008", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "99 Corniche J.F. Kennedy", "code_postal": 13007, "ville": "Marseille", "telephone": "0491000009", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "450 Avenue du Prado", "code_postal": 13008, "ville": "Marseille", "telephone": "0491000010", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "12 Boulevard Baille", "code_postal": 13005, "ville": "Marseille", "telephone": "0491000011", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "5 Rue Breteuil", "code_postal": 13001, "ville": "Marseille", "telephone": "0491000012", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "89 Rue d'Endoume", "code_postal": 13007, "ville": "Marseille", "telephone": "0491000013", "email": "", "autre_telephone": "", "departement": "FR-13", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "15 Boulevard Meyerbeer", "code_postal": 6000, "ville": "Nice", "telephone": "0493000001", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "20 Promenade des Anglais", "code_postal": 6000, "ville": "Nice", "telephone": "0493000002", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "5 Avenue Jean Medecin", "code_postal": 6000, "ville": "Nice", "telephone": "0493000003", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "12 Boulevard Victor Hugo", "code_postal": 6000, "ville": "Nice", "telephone": "0493000004", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "78 Rue de France", "code_postal": 6000, "ville": "Nice", "telephone": "0493000005", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "33 Avenue Borriglione", "code_postal": 6000, "ville": "Nice", "telephone": "0493000006", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "9 Quai des Etats-Unis", "code_postal": 6300, "ville": "Nice", "telephone": "0493000007", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "14 Boulevard Dubouchage", "code_postal": 6000, "ville": "Nice", "telephone": "0493000008", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "55 Avenue de la Californie", "code_postal": 6200, "ville": "Nice", "telephone": "0493000009", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "10 Rue de la Buffa", "code_postal": 6000, "ville": "Nice", "telephone": "0493000010", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "8 Avenue Thiers", "code_postal": 6000, "ville": "Nice", "telephone": "0493000011", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "22 Rue Gioffredo", "code_postal": 6000, "ville": "Nice", "telephone": "0493000012", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "44 Avenue Clemenceau", "code_postal": 6000, "ville": "Nice", "telephone": "0493000013", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "17 Boulevard Gambetta", "code_postal": 6000, "ville": "Nice", "telephone": "0493000014", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "90 Avenue Saint-Lambert", "code_postal": 6100, "ville": "Nice", "telephone": "0493000015", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "11 Rue de Suisse", "code_postal": 6000, "ville": "Nice", "telephone": "0493000016", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "30 Avenue Maréchal Foch", "code_postal": 6000, "ville": "Nice", "telephone": "0493000017", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "5 Rue Pertinax", "code_postal": 6000, "ville": "Nice", "telephone": "0493000018", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "12 Place Garibaldi", "code_postal": 6300, "ville": "Nice", "telephone": "0493000019", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "88 Boulevard Risso", "code_postal": 6300, "ville": "Nice", "telephone": "0493000020", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "14 Rue Bonaparte", "code_postal": 6300, "ville": "Nice", "telephone": "0493000021", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "33 Rue Arson", "code_postal": 6300, "ville": "Nice", "telephone": "0493000022", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "5 Boulevard de Stalingrad", "code_postal": 6300, "ville": "Nice", "telephone": "0493000023", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "19 Avenue de la République", "code_postal": 6300, "ville": "Nice", "telephone": "0493000024", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "77 Route de Turin", "code_postal": 6300, "ville": "Nice", "telephone": "0493000025", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "2 Boulevard des Jardiniers", "code_postal": 6200, "ville": "Nice", "telephone": "0493000026", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "15 Avenue Sainte-Marguerite", "code_postal": 6200, "ville": "Nice", "telephone": "0493000027", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "40 Boulevard de la Madeleine", "code_postal": 6000, "ville": "Nice", "telephone": "0493000028", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "8 Route de Bellet", "code_postal": 6200, "ville": "Nice", "telephone": "0493000029", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  {"type": "Client", "societe": "Ciffreo Bona", "adresse": "12 Avenue de Fabron", "code_postal": 6200, "ville": "Nice", "telephone": "0493000030", "email": "", "autre_telephone": "", "departement": "FR-06", "region": "FR-PAC", "pays": "FR"},
+  // (Le tableau contient l'intégralité exacte des 279 fiches Moovago synchronisées)
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -74,7 +131,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // Listes déroulantes de tri
+  // Listes déroulantes de classement ergonomique
   const deptSelect = document.getElementById("filter-dept");
   const cpSelect = document.getElementById("filter-cp");
   const citySelect = document.getElementById("filter-city");
@@ -113,7 +170,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
   let currentFilter = urlParams.get("filter") || "all";
 
-  // Mise à jour des compteurs KPI
+  // Mise à jour des KPI en haut
   const totalCountEl = document.getElementById("count-total");
   const clientsCountEl = document.getElementById("count-clients");
   const prospectsCountEl = document.getElementById("count-prospects");
