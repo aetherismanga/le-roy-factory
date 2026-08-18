59afa2941e2f2aa74a4d56a674097115c212f219
aetherismanga <ccjacou@gmail.com>
2026-08-18 14:33:29 +0200

    Active proximité et négoces externes sur la carte

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import "./carte-mobile-enhancements.js?v=20260818-1425";
+import "./carte-proximity.js?v=20260818-1432";
 
 // Configuration Firebase de Le Roy Factory
 const firebaseConfig = {
