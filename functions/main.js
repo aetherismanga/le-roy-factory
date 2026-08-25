fd2b91e6da27735582797007d577f3ab6064ae54
aetherismanga <ccjacou@gmail.com>
2026-08-25 13:21:10 +0200

    fix: export OTP cleanup scheduler

--- functions/main.js
+++ functions/main.js
@@ -3,4 +3,5 @@ Object.assign(functions, require("./statistics"));
 const clientVerification = require("./client-verification");
 functions.requestClientVerification = clientVerification.requestClientVerification;
 functions.verifyClientVerification = clientVerification.verifyClientVerification;
+functions.cleanupClientVerifications = clientVerification.cleanupClientVerifications;
 module.exports = functions;
