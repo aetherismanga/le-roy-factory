b428bb40a395ee4aca23827384bc0aefbaf1d8e0
aetherismanga <ccjacou@gmail.com>
2026-08-25 12:04:54 +0200

    security: export only public verification endpoints

--- functions/main.js
+++ functions/main.js
@@ -1,4 +1,6 @@
 const functions = require("./index");
 Object.assign(functions, require("./statistics"));
-Object.assign(functions, require("./client-verification"));
+const clientVerification = require("./client-verification");
+functions.requestClientVerification = clientVerification.requestClientVerification;
+functions.verifyClientVerification = clientVerification.verifyClientVerification;
 module.exports = functions;
