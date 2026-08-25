4f71fb9b04a3ad53e3cbdce4404458d28c90c451
aetherismanga <ccjacou@gmail.com>
2026-08-25 13:32:20 +0200

    feat: export Client V2 migration

--- functions/main.js
+++ functions/main.js
@@ -1,5 +1,6 @@
 const functions = require("./index");
 Object.assign(functions, require("./statistics"));
+Object.assign(functions, require("./client-schema-migration"));
 const clientVerification = require("./client-verification");
 functions.requestClientVerification = clientVerification.requestClientVerification;
 functions.verifyClientVerification = clientVerification.verifyClientVerification;
