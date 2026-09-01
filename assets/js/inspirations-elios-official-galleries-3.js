39a0d369f9c2273fe60e68d2529518804193b016
aetherismanga <ccjacou@gmail.com>
2026-09-02 01:27:34 +0200

    Ajoute Golden Hour Glow Terre Etrusche et Design Evo HD

---
+++ assets/js/inspirations-elios-official-galleries-3.js
@@ -0,0 +1,128 @@
+Object.assign(window.ELIOS_OFFICIAL_GALLERIES ||= {}, {
+  'golden-hour': [
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_BAGNO_WHITE-LADY.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_COMMERCIALE_GIN-TONIC.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_BELLINI_RISTORANTE-1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_MARGARITA_CAMERA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_ESTERNO-MOJITO_CAIPIRINHA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN_HOUR_CAIPIRINHA_CUCINA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_BAGNO_HAWAIAN_SHELL_GREY-LIGHT-copia.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_BLUE-LAGOON_SHELL_GREY-3D-SATEN_GREY-3D.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_BLUE-LAGOON-BAGNO.jpg'
+  ],
+  glow: [
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_GLOW_CALIFORNIA_BAGNO.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_GLOW_MONTANA_INGRESSO.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_GLOW_INDIANAVIRGINIA_COMMERCIALE.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_GLOW_ARIZONA_CUCINA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_GLOW_DAKOTA_LIVING.jpg'
+  ],
+  'terre-etrusche': [
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Toscana_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Toscana_2-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Toscana_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Toscana_1-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Umbria_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Umbria_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Umbria_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Umbria_1-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Umbria_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Umbria_3-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Lazio_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Lazio_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Lazio_1-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Lazio_2-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TERRE_ETRUSCHE_outdoor.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Lazio_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Lazio_4-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Marche_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Marche_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Marche_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Marche_3-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Marche_4-scaled.jpg'
+  ],
+  'd-esign-evo': [
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_ciano_A-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_ciano_B.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_ciano_mix_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_ciano_mix_2-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_senape_A_dett.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_senape_A.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_senape_B-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_Senape_mix_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_Senape_mix_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_palazzo_ducale_A.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_palazzo_ducale_BW_B_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_palazzo_ducale_BW_B.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_palazzo_ducale_BW_D.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_palazzo_ducale_BW_X.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_palazzo_ducale_BW_Z.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_palazzo_ducale_C-scaled.jpg'
+  ]
+});
+
+Object.assign(window.ELIOS_VERIFIED_VARIANTS ||= {}, {
+  'golden-hour': {
+    'White Lady': ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_BAGNO_WHITE-LADY.jpg'],
+    'Gin Tonic': ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_COMMERCIALE_GIN-TONIC.jpg'],
+    Bellini: ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_BELLINI_RISTORANTE-1.jpg'],
+    Margarita: ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_MARGARITA_CAMERA.jpg'],
+    Mojito: ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_ESTERNO-MOJITO_CAIPIRINHA.jpg'],
+    Caipirinha: [
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_ESTERNO-MOJITO_CAIPIRINHA.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN_HOUR_CAIPIRINHA_CUCINA.jpg'
+    ],
+    Hawaian: ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_BAGNO_HAWAIAN_SHELL_GREY-LIGHT-copia.jpg'],
+    'Blue Lagoon': [
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_BLUE-LAGOON_SHELL_GREY-3D-SATEN_GREY-3D.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_GOLDEN-HOUR_BLUE-LAGOON-BAGNO.jpg'
+    ]
+  },
+  glow: {
+    California: ['https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_GLOW_CALIFORNIA_BAGNO.jpg'],
+    Montana: ['https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_GLOW_MONTANA_INGRESSO.jpg'],
+    Virginia: ['https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_GLOW_INDIANAVIRGINIA_COMMERCIALE.jpg'],
+    Indiana: ['https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_GLOW_INDIANAVIRGINIA_COMMERCIALE.jpg'],
+    Arizona: ['https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_GLOW_ARIZONA_CUCINA.jpg'],
+    Dakota: ['https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_GLOW_DAKOTA_LIVING.jpg']
+  },
+  'terre-etrusche': {
+    Toscana: [
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Toscana_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Toscana_2-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Toscana_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Toscana_1-scaled.jpg'
+    ],
+    Umbria: [
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Umbria_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Umbria_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Umbria_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Umbria_1-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Umbria_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Umbria_3-scaled.jpg'
+    ],
+    Lazio: [
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Lazio_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Lazio_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Lazio_1-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Lazio_2-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Lazio_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Lazio_4-scaled.jpg'
+    ],
+    Marche: [
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_TerreEtrusche_Marche_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Marche_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Marche_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Marche_3-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/TerreEtrusche_Marche_4-scaled.jpg'
+    ]
+  },
+  'd-esign-evo': {
+    Ciano: [
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_ciano_A-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_ciano_B.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_ciano_mix_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_DesignEvo_evo_ciano_mix_2-scaled.jpg'
+    ]
+  }
+});
