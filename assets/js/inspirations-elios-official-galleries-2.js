bec410be6bcf897e64aaa6126804489d2fb8b8db
aetherismanga <ccjacou@gmail.com>
2026-09-02 01:25:27 +0200

    Ajoute Marechiaro et Hexagon en galeries HD officielles

---
+++ assets/js/inspirations-elios-official-galleries-2.js
@@ -0,0 +1,107 @@
+Object.assign(window.ELIOS_OFFICIAL_GALLERIES ||= {}, {
+  marechiaro: [
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Amalfi_amb_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Amalfi_amb_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Amalfi_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/marechiaro_amb_4.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_positano_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_positano_blu_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_positano_amb_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Sorrento_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Sorrento_amb_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Sorrento_amb_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Sorrento_amb_4.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Sorrento_amb_5.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/marechiaro_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_vietri_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_vietri_bianco_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_vietri_amb_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/marechiaro_amb_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/marechiaro_amb_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_bianco_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_amb_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_blu_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_amb_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_amb_4.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_positano_blu_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_mix_blu_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_blu_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_vietri_bianco_amb_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_mix_bianco_amb_1.jpg'
+  ],
+  hexagon: [
+    'https://eliosceramica.com/wp-content/uploads/2023/05/hexagon_frame_grey_amb.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/hexagon_cube_sky_amb.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/hexagon_lily_teal_amb.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/hexagon_lily_grey_amb.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/HEXAGON_amb_1-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/HEXAGON_amb_4-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/HEXAGON_amb_8-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/HEXAGON_amb_6-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/HEXAGON_amb_7-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/HEXAGON_amb_5-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/HEXAGON_amb_3-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/HEXAGON_amb_2-scaled.jpg'
+  ]
+});
+
+Object.assign(window.ELIOS_VERIFIED_VARIANTS ||= {}, {
+  marechiaro: {
+    Amalfi: [
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Amalfi_amb_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Amalfi_amb_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Amalfi_amb_1.jpg'
+    ],
+    Positano: [
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_positano_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_positano_blu_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_positano_amb_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_positano_blu_amb_1.jpg'
+    ],
+    Sorrento: [
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Sorrento_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Sorrento_amb_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Sorrento_amb_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Sorrento_amb_4.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_Sorrento_amb_5.jpg'
+    ],
+    Vietri: [
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_vietri_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_vietri_bianco_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_vietri_amb_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_vietri_bianco_amb_1.jpg'
+    ],
+    Mix: [
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_bianco_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_amb_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_blu_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_amb_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_amb_4.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_mix_blu_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_mix_bianco_amb_1.jpg'
+    ],
+    Blu: [
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_positano_blu_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_blu_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_positano_blu_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_mix_blu_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_blu_amb_1.jpg'
+    ],
+    Bianco: [
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_vietri_bianco_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/10/Marechiaro_mix_bianco_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_vietri_bianco_amb_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/02/Marechiaro_mix_bianco_amb_1.jpg'
+    ]
+  },
+  hexagon: {
+    Sky: ['https://eliosceramica.com/wp-content/uploads/2023/05/hexagon_cube_sky_amb.jpg'],
+    Teal: ['https://eliosceramica.com/wp-content/uploads/2023/05/hexagon_lily_teal_amb.jpg'],
+    Grey: [
+      'https://eliosceramica.com/wp-content/uploads/2023/05/hexagon_frame_grey_amb.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2023/05/hexagon_lily_grey_amb.jpg'
+    ]
+  }
+});
