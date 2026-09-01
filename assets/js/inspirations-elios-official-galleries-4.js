83a6017b095d0c232dc933322f6c5f6f513a5fcf
aetherismanga <ccjacou@gmail.com>
2026-09-02 01:29:34 +0200

    Ajoute Twist Venere et Shell en galeries HD

---
+++ assets/js/inspirations-elios-official-galleries-4.js
@@ -0,0 +1,79 @@
+Object.assign(window.ELIOS_OFFICIAL_GALLERIES ||= {}, {
+  twist: [
+    'https://eliosceramica.com/wp-content/uploads/2024/01/twist_amb_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/01/twist_amb_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/01/twist_amb_5.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/01/twist_amb_4.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/01/twist_amb_1.jpg'
+  ],
+  venere: [
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO_ATLANTE_PERLA-MATERICA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_NUOVO-BAGNO_PERLA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO_AURA_AURA-MATERICA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_COMMERCIALE-FLORA_RUGIADA-MATERICA_AURA-DORICA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_NUOVO-BAGNO-SETA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_COMMERCIALE-FLORA_RUGIADA-MATERICA_AURA-DORICA-1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_SCORCIO-OPTICA-ROSA_FLORA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO_RUGIADA_RUGIADA-MATERICA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO-ZEFIRO_DORICA-ZEFIRO.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_NUOVO-BAGNO-MARE.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO_ATLANTE_PERLA-MATERICA-1.jpg'
+  ],
+  shell: [
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_IVORY-3D-SATEN_BAGNO-2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_BEIGE-3D-MATT_CAMERA-1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL-BEIGE-copia.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_BEIGE-3D-MATT_LIVING-1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_GREY-3D-SATEN_GREY-3D.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_COMMERCIALE-IVORY-LIGHT.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_-CUCINA_SHELL_BEIGE-LIGHT.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_OUTDOOR_BEIGE-LIGHT.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_BAGNO_SHELL_GREY-LIGHT.jpg'
+  ]
+});
+
+Object.assign(window.ELIOS_VERIFIED_VARIANTS ||= {}, {
+  venere: {
+    Perla: [
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO_ATLANTE_PERLA-MATERICA.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_NUOVO-BAGNO_PERLA.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO_ATLANTE_PERLA-MATERICA-1.jpg'
+    ],
+    Aura: [
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO_AURA_AURA-MATERICA.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_COMMERCIALE-FLORA_RUGIADA-MATERICA_AURA-DORICA.jpg'
+    ],
+    Seta: ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_NUOVO-BAGNO-SETA.jpg'],
+    Flora: [
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_COMMERCIALE-FLORA_RUGIADA-MATERICA_AURA-DORICA.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_COMMERCIALE-FLORA_RUGIADA-MATERICA_AURA-DORICA-1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_SCORCIO-OPTICA-ROSA_FLORA.jpg'
+    ],
+    Rugiada: [
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_COMMERCIALE-FLORA_RUGIADA-MATERICA_AURA-DORICA.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_COMMERCIALE-FLORA_RUGIADA-MATERICA_AURA-DORICA-1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO_RUGIADA_RUGIADA-MATERICA.jpg'
+    ],
+    Zefiro: ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO-ZEFIRO_DORICA-ZEFIRO.jpg'],
+    Mare: ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_NUOVO-BAGNO-MARE.jpg'],
+    Atlante: [
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO_ATLANTE_PERLA-MATERICA.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_VENERE_BAGNO_ATLANTE_PERLA-MATERICA-1.jpg'
+    ]
+  },
+  shell: {
+    Ivory: ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_IVORY-3D-SATEN_BAGNO-2.jpg'],
+    Beige: [
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_BEIGE-3D-MATT_CAMERA-1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL-BEIGE-copia.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_BEIGE-3D-MATT_LIVING-1.jpg'
+    ],
+    Grey: ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_GREY-3D-SATEN_GREY-3D.jpg'],
+    'Ivory Light': ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_COMMERCIALE-IVORY-LIGHT.jpg'],
+    'Beige Light': [
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_-CUCINA_SHELL_BEIGE-LIGHT.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_SHELL_OUTDOOR_BEIGE-LIGHT.jpg'
+    ],
+    'Grey Light': ['https://eliosceramica.com/wp-content/uploads/2025/10/ELIOS_BAGNO_SHELL_GREY-LIGHT.jpg']
+  }
+});
