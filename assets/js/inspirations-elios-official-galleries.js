3ae43c8cd33eb9acd2503253ed12edf55b58b8be
aetherismanga <ccjacou@gmail.com>
2026-09-02 01:21:38 +0200

    Ajoute les galeries HD Grand Place Brooklyn et Clay

---
+++ assets/js/inspirations-elios-official-galleries.js
@@ -0,0 +1,111 @@
+window.ELIOS_OFFICIAL_GALLERIES = {
+  'grand-place': [
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_4.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_5.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_1-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_4.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_5.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_6.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_7-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_8.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/GrandPlace_namur_DecWarm_Esterno.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Mons_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Mons_2.jpg'
+  ],
+  brooklyn: [
+    'https://eliosceramica.com/wp-content/uploads/2021/11/BROOKLYN_LVING_white.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/BROOKLYN_CAMERA_beige.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Brooklyn_Beige_Interno_Esterno.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Brooklyn_beige_amb.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Brooklyn_Beige_Vassoio.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/BROOKLYN_LIVING_taupe.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Brooklyn_Taupe_Esterno.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Brooklyn_Grey_PubblicoZenitale.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/BROOKLYN_BAR_dark_grey-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_BROOKLYN_ALMOND_LIVING.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_BROOKLYN_GOLD_BAGNO.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_BROOKLYN_GOLD_IN_OUT.jpg'
+  ],
+  clay: [
+    'https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_BAR_COTTON_EMERALD.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_RISTORANTE_OCEAN_MUSTARD-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Clay_Ocean_Bouquet_Cotton.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_BAGNO_LINEN.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_CAMERA_BOUQUET.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_bouquet_amb.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_CUCINA_SILK.jpg'
+  ]
+};
+
+window.ELIOS_VERIFIED_VARIANTS = {
+  'grand-place': {
+    Bruxelles: [
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_4.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_5.jpg'
+    ],
+    Antwerpen: [
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_1-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_4.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_5.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_6.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_7-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_8.jpg'
+    ],
+    Namur: [
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/GrandPlace_namur_DecWarm_Esterno.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_1.jpg'
+    ]
+  },
+  brooklyn: {
+    White: ['https://eliosceramica.com/wp-content/uploads/2021/11/BROOKLYN_LVING_white.jpg'],
+    Beige: [
+      'https://eliosceramica.com/wp-content/uploads/2021/11/BROOKLYN_CAMERA_beige.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Brooklyn_Beige_Interno_Esterno.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Brooklyn_beige_amb.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Brooklyn_Beige_Vassoio.jpg'
+    ],
+    Taupe: [
+      'https://eliosceramica.com/wp-content/uploads/2021/11/BROOKLYN_LIVING_taupe.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Brooklyn_Taupe_Esterno.jpg'
+    ],
+    Grey: ['https://eliosceramica.com/wp-content/uploads/2021/11/Brooklyn_Grey_PubblicoZenitale.jpg'],
+    Dark: ['https://eliosceramica.com/wp-content/uploads/2021/11/BROOKLYN_BAR_dark_grey-scaled.jpg'],
+    Almond: ['https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_BROOKLYN_ALMOND_LIVING.jpg'],
+    Gold: [
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_BROOKLYN_GOLD_BAGNO.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_BROOKLYN_GOLD_IN_OUT.jpg'
+    ]
+  },
+  clay: {
+    Emerald: ['https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_BAR_COTTON_EMERALD.jpg'],
+    Cotton: [
+      'https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_BAR_COTTON_EMERALD.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Clay_Ocean_Bouquet_Cotton.jpg'
+    ],
+    Ocean: [
+      'https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_RISTORANTE_OCEAN_MUSTARD-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Clay_Ocean_Bouquet_Cotton.jpg'
+    ],
+    Mustard: ['https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_RISTORANTE_OCEAN_MUSTARD-scaled.jpg'],
+    Linen: ['https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_BAGNO_LINEN.jpg'],
+    Bouquet: [
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Clay_Ocean_Bouquet_Cotton.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_CAMERA_BOUQUET.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_bouquet_amb.jpg'
+    ],
+    Silk: ['https://eliosceramica.com/wp-content/uploads/2021/11/CLAY_CUCINA_SILK.jpg']
+  }
+};
