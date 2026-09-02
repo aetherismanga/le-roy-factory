67a94cece8ba858adcbe098764279c8f8dfd4734
aetherismanga <ccjacou@gmail.com>
2026-09-02 13:50:48 +0200

    Ajuste les visuels Sedimenti par couleur selon la galerie officielle

---
+++ assets/js/inspirations-elios-official-galleries-6.js
@@ -0,0 +1,48 @@
+// Ajustements couleur vérifiés directement sur la galerie officielle Elios.
+Object.assign(window.ELIOS_OFFICIAL_GALLERIES ||= {}, {
+  sedimenti: [
+    'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-BEIGE-TUMBLED-1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/EL_SEDIMENTI_BEIGE_LIRERIA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_BEIGE_COMMERCIALE.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_INGRESSO_beige_white.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-GREY-TUMBLED.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_WHITE_GREY_IN_OUT.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CUCINA_white_grey.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-SAND-TUMBLED.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_STICKS_BAGNO_sand.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CAMERA_sand_white.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-BEIGE-TUMBLED.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_WHITE_SAND_BAGNO.jpg'
+  ]
+});
+
+Object.assign(window.ELIOS_VERIFIED_VARIANTS ||= {}, {
+  sedimenti: {
+    Beige: [
+      'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-BEIGE-TUMBLED-1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/EL_SEDIMENTI_BEIGE_LIRERIA.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_BEIGE_COMMERCIALE.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_INGRESSO_beige_white.jpg'
+    ],
+    Grey: [
+      'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-GREY-TUMBLED.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_WHITE_GREY_IN_OUT.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CUCINA_white_grey.jpg'
+    ],
+    Sand: [
+      'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-SAND-TUMBLED.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/EL_SEDIMENTI_BEIGE_LIRERIA.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_BEIGE_COMMERCIALE.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_STICKS_BAGNO_sand.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CAMERA_sand_white.jpg'
+    ],
+    White: [
+      'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-BEIGE-TUMBLED.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_WHITE_SAND_BAGNO.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_WHITE_GREY_IN_OUT.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_INGRESSO_beige_white.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CUCINA_white_grey.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CAMERA_sand_white.jpg'
+    ]
+  }
+});
