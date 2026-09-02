5a030c6ee1d6a40d66c6edd7561575e13b18aa52
aetherismanga <ccjacou@gmail.com>
2026-09-02 13:42:54 +0200

    Complète les séries pierre Elios en galeries HD officielles

---
+++ assets/js/inspirations-elios-official-galleries-5.js
@@ -0,0 +1,162 @@
+// Lot pierre ELIOS - sources natives du site officiel uniquement.
+// Aucune miniature locale compressée n'est utilisée pour ces collections.
+Object.assign(window.ELIOS_OFFICIAL_GALLERIES ||= {}, {
+  'bavaria-stone': [
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_Bavaria_Stone_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_Bavaria_Stone_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_Bavaria_Stone_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/BAVARIA-STONE_White_beige_grey.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/bavaria_stone_noce_amb1-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/bavaria_stone_noce_amb3-scaled.jpg'
+  ],
+  dolomiti: [
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_bianco_greige.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_CAMERA_beige.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_BAGNO_beige.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_beige_lappato.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_beige1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_CUCINA_greige.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_CAMERAgreige.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_CAMERA_greige_chevron.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_greige.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_greige_lappato.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_grigio.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_grigio_chevron.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_grigio1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_antracite.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_antracite_out.jpg'
+  ],
+  'grand-place': [
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_4.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_5.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_1-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_4.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_5.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_6.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_7-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_8.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/GrandPlace_namur_DecWarm_Esterno.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_1.jpg'
+  ],
+  harmony: [
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_White_1.jpg'
+  ],
+  'millennium-quartz': [
+    'https://eliosceramica.com/wp-content/uploads/2023/10/millenium_quartz_amb_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/millenium_quartz_amb_4.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/millenium_quartz_amb_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/millenium_quartz_amb_5.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/10/millenium_quartz_amb_1.jpg'
+  ],
+  sedimenti: [
+    'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-BEIGE-TUMBLED-1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/EL_SEDIMENTI_BEIGE_LIRERIA.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_BEIGE_COMMERCIALE.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_INGRESSO_beige_white.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-GREY-TUMBLED.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_WHITE_GREY_IN_OUT.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CUCINA_white_grey.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-SAND-TUMBLED.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_STICKS_BAGNO_sand.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CAMERA_sand_white.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_WHITE_SAND_BAGNO.jpg'
+  ]
+});
+
+Object.assign(window.ELIOS_VERIFIED_VARIANTS ||= {}, {
+  'bavaria-stone': {
+    'Bavaria Stone': [
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_Bavaria_Stone_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_Bavaria_Stone_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_Bavaria_Stone_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/BAVARIA-STONE_White_beige_grey.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/bavaria_stone_noce_amb1-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/bavaria_stone_noce_amb3-scaled.jpg'
+    ]
+  },
+  dolomiti: {
+    Bianco: ['https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_bianco_greige.jpg'],
+    Beige: [
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_CAMERA_beige.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_BAGNO_beige.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_beige_lappato.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_beige1.jpg'
+    ],
+    Greige: [
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_bianco_greige.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_CUCINA_greige.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_CAMERAgreige.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_CAMERA_greige_chevron.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_greige.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_greige_lappato.jpg'
+    ],
+    Grigio: [
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_grigio.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_grigio_chevron.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_grigio1.jpg'
+    ],
+    Antracite: [
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_antracite.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/DOLOMITI_LIVING_antracite_out.jpg'
+    ]
+  },
+  'grand-place': {
+    Bruxelles: [
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_4.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Bruxelles_5.jpg'
+    ],
+    Antwerpen: [
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_1-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_4.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_5.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_6.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_7-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Antwerpen_8.jpg'
+    ],
+    Namur: [
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/GrandPlace_namur_DecWarm_Esterno.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/01/Elios_GrandPlace_Namur_1.jpg'
+    ]
+  },
+  harmony: {
+    White: ['https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_White_1.jpg']
+  },
+  sedimenti: {
+    Beige: [
+      'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-BEIGE-TUMBLED-1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/EL_SEDIMENTI_BEIGE_LIRERIA.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_BEIGE_COMMERCIALE.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_INGRESSO_beige_white.jpg'
+    ],
+    Grey: [
+      'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-GREY-TUMBLED.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_WHITE_GREY_IN_OUT.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CUCINA_white_grey.jpg'
+    ],
+    Sand: [
+      'https://eliosceramica.com/wp-content/uploads/2024/10/SEDIMENTI-MODULO-SAND-TUMBLED.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_STICKS_BAGNO_sand.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CAMERA_sand_white.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_WHITE_SAND_BAGNO.jpg'
+    ],
+    White: [
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_INGRESSO_beige_white.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_WHITE_GREY_IN_OUT.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CUCINA_white_grey.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_TAGLI_CAMERA_sand_white.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2024/10/ELIOS_SEDIMENTI_WHITE_SAND_BAGNO.jpg'
+    ]
+  }
+});
