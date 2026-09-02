a000ff9f81d408317f52cf7ef3250805543899a3
aetherismanga <ccjacou@gmail.com>
2026-09-02 10:28:23 +0200

    Point Slate galleries to valid JPEG assets

--- assets/js/inspirations-elios-slate-hd.js
+++ assets/js/inspirations-elios-slate-hd.js
@@ -1,21 +1,21 @@
 Object.assign(window.ELIOS_OFFICIAL_GALLERIES ||= {}, {
   slate: [
-    'assets/img/elios/slate/slate-river-hd.svg',
-    'assets/img/elios/slate/slate-dark-hd.svg',
-    'assets/img/elios/slate/slate-flame-hd.svg',
-    'assets/img/elios/slate/slate-natural-hd.svg',
-    'assets/img/elios/slate/slate-gold-hd.svg',
-    'assets/img/elios/slate/slate-grey-hd.svg'
+    'assets/img/elios/slate/slate-river-hd.jpg',
+    'assets/img/elios/slate/slate-dark-hd.jpg',
+    'assets/img/elios/slate/slate-flame-hd.jpg',
+    'assets/img/elios/slate/slate-natural-hd.jpg',
+    'assets/img/elios/slate/slate-gold-hd.jpg',
+    'assets/img/elios/slate/slate-grey-hd.jpg'
   ]
 });
 
 Object.assign(window.ELIOS_VERIFIED_VARIANTS ||= {}, {
   slate: {
-    River: ['assets/img/elios/slate/slate-river-hd.svg'],
-    Dark: ['assets/img/elios/slate/slate-dark-hd.svg'],
-    Flame: ['assets/img/elios/slate/slate-flame-hd.svg'],
-    Natural: ['assets/img/elios/slate/slate-natural-hd.svg'],
-    Gold: ['assets/img/elios/slate/slate-gold-hd.svg'],
-    Grey: ['assets/img/elios/slate/slate-grey-hd.svg']
+    River: ['assets/img/elios/slate/slate-river-hd.jpg'],
+    Dark: ['assets/img/elios/slate/slate-dark-hd.jpg'],
+    Flame: ['assets/img/elios/slate/slate-flame-hd.jpg'],
+    Natural: ['assets/img/elios/slate/slate-natural-hd.jpg'],
+    Gold: ['assets/img/elios/slate/slate-gold-hd.jpg'],
+    Grey: ['assets/img/elios/slate/slate-grey-hd.jpg']
   }
 });
