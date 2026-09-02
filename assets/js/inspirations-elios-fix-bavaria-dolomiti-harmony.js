d923b07cf7409ecd218a0142b8e9be3546f37706
aetherismanga <ccjacou@gmail.com>
2026-09-02 14:37:47 +0200

    Corrige Bavaria Dolomiti et complète Harmony

---
+++ assets/js/inspirations-elios-fix-bavaria-dolomiti-harmony.js
@@ -0,0 +1,81 @@
+// Correctif prioritaire ELIOS — 2026-09-02
+// Chargé après les lots de galeries précédents afin de remplacer les chemins cassés.
+Object.assign(window.ELIOS_OFFICIAL_GALLERIES ||= {}, {
+  'bavaria-stone': [
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Bavaria_Stone_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Bavaria_Stone_2.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Bavaria_Stone_3.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/BAVARIA-STONE_White_beige_grey.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/bavaria_stone_noce_amb1-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/bavaria_stone_noce_amb3-scaled.jpg'
+  ],
+  dolomiti: [
+    'https://eliosceramica.com/wp-content/uploads/2021/11/DOLOMITI_LIVING_bianco_greige.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/DOLOMITI_CAMERA_beige.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Beige_Ristorante_Interno_Esterno.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Beige_Ristorante_Dett_Esterno.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Beige_Ristorante_Dett_Interno.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/DOLOMITI_LIVING_greige.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_dettaglio.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Greige_Sgabello.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Pietra.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/DOLOMITI_BAGNO_grigio.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Greige_Grigio_Esterno_Dett.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_greige_Grigio_Esterno.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Grigio_In_Out_Dett.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/DOLOMITI_CUCINA_antracite.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_antracite_amb.jpg'
+  ],
+  harmony: [
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_White_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_Grey_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_Black_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_Cream_1.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_Taupe_1.jpg'
+  ]
+});
+
+Object.assign(window.ELIOS_VERIFIED_VARIANTS ||= {}, {
+  'bavaria-stone': {
+    'Bavaria Stone': [
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Bavaria_Stone_1.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Bavaria_Stone_2.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Bavaria_Stone_3.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/BAVARIA-STONE_White_beige_grey.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/bavaria_stone_noce_amb1-scaled.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/02/bavaria_stone_noce_amb3-scaled.jpg'
+    ]
+  },
+  dolomiti: {
+    Bianco: ['https://eliosceramica.com/wp-content/uploads/2021/11/DOLOMITI_LIVING_bianco_greige.jpg'],
+    Beige: [
+      'https://eliosceramica.com/wp-content/uploads/2021/11/DOLOMITI_CAMERA_beige.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Beige_Ristorante_Interno_Esterno.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Beige_Ristorante_Dett_Esterno.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Beige_Ristorante_Dett_Interno.jpg'
+    ],
+    Greige: [
+      'https://eliosceramica.com/wp-content/uploads/2021/11/DOLOMITI_LIVING_bianco_greige.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/DOLOMITI_LIVING_greige.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_dettaglio.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Greige_Sgabello.jpg'
+    ],
+    Grigio: [
+      'https://eliosceramica.com/wp-content/uploads/2021/11/DOLOMITI_BAGNO_grigio.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Greige_Grigio_Esterno_Dett.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_greige_Grigio_Esterno.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_Grigio_In_Out_Dett.jpg'
+    ],
+    Antracite: [
+      'https://eliosceramica.com/wp-content/uploads/2021/11/DOLOMITI_CUCINA_antracite.jpg',
+      'https://eliosceramica.com/wp-content/uploads/2021/11/Dolomiti_antracite_amb.jpg'
+    ]
+  },
+  harmony: {
+    White: ['https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_White_1.jpg'],
+    Grey: ['https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_Grey_1.jpg'],
+    Black: ['https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_Black_1.jpg'],
+    Cream: ['https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_Cream_1.jpg'],
+    Taupe: ['https://eliosceramica.com/wp-content/uploads/2021/02/Elios_Harmony_Taupe_1.jpg']
+  }
+});
