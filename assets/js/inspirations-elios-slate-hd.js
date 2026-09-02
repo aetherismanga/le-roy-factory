22a6e57c08d831644ea1ee4e12fdf1fe57ccbb01
aetherismanga <ccjacou@gmail.com>
2026-09-02 11:55:05 +0200

    Use full-resolution Slate source visuals without downscaling

--- assets/js/inspirations-elios-slate-hd.js
+++ assets/js/inspirations-elios-slate-hd.js
@@ -1,21 +1,36 @@
 Object.assign(window.ELIOS_OFFICIAL_GALLERIES ||= {}, {
   slate: [
-    'assets/img/elios/slate/slate-river-hd.jpg',
-    'assets/img/elios/slate/slate-dark-hd.jpg',
-    'assets/img/elios/slate/slate-flame-hd.jpg',
-    'assets/img/elios/slate/slate-natural-hd.jpg',
-    'assets/img/elios/slate/slate-gold-hd.jpg',
-    'assets/img/elios/slate/slate-grey-hd.jpg'
+    'https://www.fliesengigant.de/out/pictures/master/product/1/03f60606.jpg',
+    'https://www.fliesengigant.de/out/pictures/master/product/4/ambiente3_dark.jpeg',
+    'https://www.fliesengigant.de/out/pictures/master/product/4/slate_flame_cortile_04_1copia.jpg',
+    'https://id-sol.fr/cdn/shop/files/slate_amb_7-scaled_1024x1024.jpg?v=1751123554',
+    'https://www.fliesengigant.de/out/pictures/generated/product/4/1000_1000_75/stilllife_slate_gold60x120_30x60%281%29.jpg',
+    'https://fliseshoppen.dk/wp-content/uploads/2022/06/Slate-Grey-60x60-2.jpg'
   ]
 });
 
 Object.assign(window.ELIOS_VERIFIED_VARIANTS ||= {}, {
   slate: {
-    River: ['assets/img/elios/slate/slate-river-hd.jpg'],
-    Dark: ['assets/img/elios/slate/slate-dark-hd.jpg'],
-    Flame: ['assets/img/elios/slate/slate-flame-hd.jpg'],
-    Natural: ['assets/img/elios/slate/slate-natural-hd.jpg'],
-    Gold: ['assets/img/elios/slate/slate-gold-hd.jpg'],
-    Grey: ['assets/img/elios/slate/slate-grey-hd.jpg']
+    River: [
+      'https://www.fliesengigant.de/out/pictures/master/product/1/03f60606.jpg',
+      'https://www.fliesengigant.de/out/pictures/master/product/3/03f60602.jpg'
+    ],
+    Dark: [
+      'https://www.fliesengigant.de/out/pictures/master/product/4/ambiente3_dark.jpeg',
+      'https://www.fliesengigant.de/out/pictures/generated/product/1/1000_1000_75/03f60903.jpg'
+    ],
+    Flame: [
+      'https://www.fliesengigant.de/out/pictures/master/product/4/slate_flame_cortile_04_1copia.jpg'
+    ],
+    Natural: [
+      'https://id-sol.fr/cdn/shop/files/slate_amb_7-scaled_1024x1024.jpg?v=1751123554',
+      'https://www.fliesengigant.de/out/pictures/generated/product/4/1000_1000_75/03f36004.jpg'
+    ],
+    Gold: [
+      'https://www.fliesengigant.de/out/pictures/generated/product/4/1000_1000_75/stilllife_slate_gold60x120_30x60%281%29.jpg'
+    ],
+    Grey: [
+      'https://fliseshoppen.dk/wp-content/uploads/2022/06/Slate-Grey-60x60-2.jpg'
+    ]
   }
 });
