e80c140842b8fc10c8b5f5d2c854c7fa86366dac
aetherismanga <ccjacou@gmail.com>
2026-09-02 09:02:29 +0200

    Prépare Slate HD officiel par couleur

---
+++ assets/js/inspirations-elios-slate-hd.js
@@ -0,0 +1,18 @@
+Object.assign(window.ELIOS_OFFICIAL_GALLERIES ||= {}, {
+  slate: [
+    'https://eliosceramica.com/wp-content/uploads/2023/05/slate_amb_2-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/slate_amb_1-scaled.jpg',
+    'https://eliosceramica.com/wp-content/uploads/2023/05/slate_amb_3-scaled.jpg'
+  ]
+});
+
+Object.assign(window.ELIOS_VERIFIED_VARIANTS ||= {}, {
+  slate: {
+    River: ['https://eliosceramica.com/wp-content/uploads/2023/05/slate_amb_2-scaled.jpg'],
+    Dark: ['https://eliosceramica.com/wp-content/uploads/2023/05/slate_amb_1-scaled.jpg'],
+    Flame: ['https://eliosceramica.com/wp-content/uploads/2023/05/slate_amb_3-scaled.jpg'],
+    Natural: ['https://eliosceramica.com/wp-content/uploads/2023/05/slate_amb_3-scaled.jpg'],
+    Gold: ['https://eliosceramica.com/wp-content/uploads/2023/05/slate_amb_1-scaled.jpg'],
+    Grey: ['https://eliosceramica.com/wp-content/uploads/2023/05/slate_amb_1-scaled.jpg']
+  }
+});
