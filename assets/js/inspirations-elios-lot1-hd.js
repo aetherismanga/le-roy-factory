c69c79a4451e4933481ebee54f4a4128c879424b
aetherismanga <ccjacou@gmail.com>
2026-09-02 20:10:25 +0000

    Corrige Domus Allure Creta Deco et Segmento en HD officiel

---
+++ assets/js/inspirations-elios-lot1-hd.js
@@ -0,0 +1,146 @@
+// ELIOS lot 1 HD — sources officielles uniquement, fichiers natifs sans recompression.
+Object.assign(window.ELIOS_OFFICIAL_GALLERIES ||= {}, {
+  "domus": [
+    "assets/img/elios/domus/lot1-01.jpg",
+    "assets/img/elios/domus/lot1-02.jpg",
+    "assets/img/elios/domus/lot1-03.jpg",
+    "assets/img/elios/domus/lot1-04.jpg",
+    "assets/img/elios/domus/lot1-05.jpg",
+    "assets/img/elios/domus/lot1-06.jpg"
+  ],
+  "allure": [
+    "assets/img/elios/allure/lot1-01.jpg",
+    "assets/img/elios/allure/lot1-02.jpg",
+    "assets/img/elios/allure/lot1-03.jpg",
+    "assets/img/elios/allure/lot1-04.jpg",
+    "assets/img/elios/allure/lot1-05.jpg",
+    "assets/img/elios/allure/lot1-06.jpg",
+    "assets/img/elios/allure/lot1-07.jpg",
+    "assets/img/elios/allure/lot1-08.jpg",
+    "assets/img/elios/allure/lot1-09.jpg",
+    "assets/img/elios/allure/lot1-10.jpg"
+  ],
+  "creta": [
+    "assets/img/elios/creta/lot1-01.jpg",
+    "assets/img/elios/creta/lot1-02.jpg",
+    "assets/img/elios/creta/lot1-03.jpg",
+    "assets/img/elios/creta/lot1-04.jpg",
+    "assets/img/elios/creta/lot1-05.jpg",
+    "assets/img/elios/creta/lot1-06.jpg",
+    "assets/img/elios/creta/lot1-07.png",
+    "assets/img/elios/creta/lot1-08.jpg"
+  ],
+  "deco": [
+    "assets/img/elios/deco/lot1-01.jpg",
+    "assets/img/elios/deco/lot1-02.jpg",
+    "assets/img/elios/deco/lot1-03.jpg",
+    "assets/img/elios/deco/lot1-04.jpg",
+    "assets/img/elios/deco/lot1-05.jpg",
+    "assets/img/elios/deco/lot1-06.jpg",
+    "assets/img/elios/deco/lot1-07.jpg",
+    "assets/img/elios/deco/lot1-08.jpg",
+    "assets/img/elios/deco/lot1-09.jpg",
+    "assets/img/elios/deco/lot1-10.jpg",
+    "assets/img/elios/deco/lot1-11.jpg",
+    "assets/img/elios/deco/lot1-12.jpg",
+    "assets/img/elios/deco/lot1-13.jpg",
+    "assets/img/elios/deco/lot1-14.jpg"
+  ],
+  "segmento": [
+    "assets/img/elios/segmento/lot1-01.jpg",
+    "assets/img/elios/segmento/lot1-02.jpg",
+    "assets/img/elios/segmento/lot1-03.jpg",
+    "assets/img/elios/segmento/lot1-04.jpg",
+    "assets/img/elios/segmento/lot1-05.jpg",
+    "assets/img/elios/segmento/lot1-06.jpg",
+    "assets/img/elios/segmento/lot1-07.jpg",
+    "assets/img/elios/segmento/lot1-08.jpg"
+  ]
+});
+
+Object.assign(window.ELIOS_VERIFIED_VARIANTS ||= {}, {
+  "domus": {
+    "Canyon Rust": [
+      "assets/img/elios/domus/lot1-05.jpg",
+      "assets/img/elios/domus/lot1-06.jpg"
+    ],
+    "Forest Green": [
+      "assets/img/elios/domus/lot1-04.jpg"
+    ],
+    "Clay Red": [
+      "assets/img/elios/domus/lot1-03.jpg"
+    ],
+    "Ocean Blue": [
+      "assets/img/elios/domus/lot1-01.jpg"
+    ],
+    "Moon White": [
+      "assets/img/elios/domus/lot1-01.jpg"
+    ],
+    "Sunset Rose": [
+      "assets/img/elios/domus/lot1-02.jpg"
+    ]
+  },
+  "allure": {
+    "Eden": [
+      "assets/img/elios/allure/lot1-02.jpg"
+    ]
+  },
+  "creta": {
+    "Zucchero": [
+      "assets/img/elios/creta/lot1-06.jpg"
+    ],
+    "Matcha": [
+      "assets/img/elios/creta/lot1-06.jpg"
+    ],
+    "Avena": [
+      "assets/img/elios/creta/lot1-01.jpg",
+      "assets/img/elios/creta/lot1-02.jpg"
+    ],
+    "Biscotto": [
+      "assets/img/elios/creta/lot1-01.jpg"
+    ],
+    "Caramello": [
+      "assets/img/elios/creta/lot1-03.jpg",
+      "assets/img/elios/creta/lot1-04.jpg"
+    ],
+    "Ginepro": [
+      "assets/img/elios/creta/lot1-05.jpg"
+    ],
+    "Pepe": [
+      "assets/img/elios/creta/lot1-08.jpg"
+    ]
+  },
+  "deco": {
+    "Taupe": [
+      "assets/img/elios/deco/lot1-13.jpg"
+    ],
+    "Light Blue": [
+      "assets/img/elios/deco/lot1-03.jpg",
+      "assets/img/elios/deco/lot1-04.jpg",
+      "assets/img/elios/deco/lot1-06.jpg"
+    ]
+  },
+  "segmento": {
+    "Albus": [
+      "assets/img/elios/segmento/lot1-01.jpg"
+    ],
+    "Nigrum": [
+      "assets/img/elios/segmento/lot1-04.jpg"
+    ],
+    "Flavus": [
+      "assets/img/elios/segmento/lot1-05.jpg"
+    ],
+    "Lapis": [
+      "assets/img/elios/segmento/lot1-06.jpg"
+    ],
+    "Duplex A": [
+      "assets/img/elios/segmento/lot1-07.jpg"
+    ],
+    "Duplex B": [
+      "assets/img/elios/segmento/lot1-08.jpg"
+    ],
+    "Duplex C": [
+      "assets/img/elios/segmento/lot1-03.jpg"
+    ]
+  }
+});
