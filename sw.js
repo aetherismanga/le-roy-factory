923dbd378427cdf106e876657c98b9c9ca82d34b
aetherismanga <ccjacou@gmail.com>
2026-09-03 20:47:31 +0200

    chore: renouvelle le cache PWA après correction du logo mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lrf-pwa-v4-agenda';
+const CACHE='lrf-pwa-v5-dashboard-logo';
 const CORE=['/','/index.html','/assets/brand-v2/assetlogorond.png'];
 
 self.addEventListener('install',event=>{
