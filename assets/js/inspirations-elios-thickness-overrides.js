692df5e3fde5b99584ae05d7b3b1a19d9f5aa9e3
aetherismanga <ccjacou@gmail.com>
2026-09-07 06:14:40 +0200

    ELIOS lot 4: refresh stock button script version

--- assets/js/inspirations-elios-thickness-overrides.js
+++ assets/js/inspirations-elios-thickness-overrides.js
@@ -82,7 +82,7 @@
   // Disponibilités ELIOS : Roma + séries activées par lots.
   if (!document.querySelector('script[data-lrf-elios-stock-roma]')) {
     const stockScript = document.createElement('script');
-    stockScript.src = 'assets/js/elios-stock-roma-test.js?v=20260907-lot3';
+    stockScript.src = 'assets/js/elios-stock-roma-test.js?v=20260907-lot4';
     stockScript.defer = true;
     stockScript.dataset.lrfEliosStockRoma = '1';
     document.head.appendChild(stockScript);
