28a18973960037b618a50d5ec35686fe1575ef8c
aetherismanga <ccjacou@gmail.com>
2026-09-07 23:40:58 +0200

    Expose la programmation automatique BILT à 72h

--- functions/entry.js
+++ functions/entry.js
@@ -8,8 +8,9 @@ const eliosOrder = require('./elios-order');
 const eliosCatalog = require('./elios-catalog-api');
 const biltOrder = require('./bilt-order');
 const biltAutoTask = require('./bilt-auto-task');
+const biltSchedule = require('./bilt-schedule');
 
-Object.assign(exports, base, tariffAccess, proSession, lrfAnalytics, eliosStock, eliosOrder, eliosCatalog, biltOrder, biltAutoTask);
+Object.assign(exports, base, tariffAccess, proSession, lrfAnalytics, eliosStock, eliosOrder, eliosCatalog, biltOrder, biltAutoTask, biltSchedule);
 
 // La migration vers Storage privé est terminée. Cet endpoint temporaire reste
 // présent uniquement pour éviter une suppression de fonction lors du déploiement,
