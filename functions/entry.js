236f6f3980548b70a21933fc58ccfc3766174676
aetherismanga <ccjacou@gmail.com>
2026-09-10 23:50:55 +0200

    feat: export Elios pose order endpoint

--- functions/entry.js
+++ functions/entry.js
@@ -5,12 +5,13 @@ const proSession = require('./pro-session');
 const lrfAnalytics = require('./lrf-analytics');
 const eliosStock = require('./elios-stock');
 const eliosOrder = require('./elios-order');
+const eliosPoseOrder = require('./elios-pose-order');
 const eliosCatalog = require('./elios-catalog-api');
 const biltOrder = require('./bilt-router');
 const biltAutoTask = require('./bilt-auto-task');
 const biltSchedule = require('./bilt-schedule');
 
-Object.assign(exports, base, tariffAccess, proSession, lrfAnalytics, eliosStock, eliosOrder, eliosCatalog, biltOrder, biltAutoTask, biltSchedule);
+Object.assign(exports, base, tariffAccess, proSession, lrfAnalytics, eliosStock, eliosOrder, eliosPoseOrder, eliosCatalog, biltOrder, biltAutoTask, biltSchedule);
 
 // La migration vers Storage privé est terminée. Cet endpoint temporaire reste
 // présent uniquement pour éviter une suppression de fonction lors du déploiement,
