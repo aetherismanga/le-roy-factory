51391627681ff4fd896552febf9359f424e40098
aetherismanga <ccjacou@gmail.com>
2026-09-14 13:07:09 +0200

    Charge le design allégé Documents clients

--- assets/js/crm-settings-nav.js
+++ assets/js/crm-settings-nav.js
@@ -1,2 +1,3 @@
 import('./crm-settings-nav-core.js?v=20260914-client-docs1').catch(err=>console.error('Navigation Paramètres :',err));
 import('./settings-documents-clients-card.js?v=20260914-client-docs1').catch(err=>console.error('Documents clients :',err));
+import('./documents-clients-light.js?v=20260914-light1').catch(err=>console.error('Design Documents clients :',err));
